8345373c6747c0f4565fce31dbdd9070d52c4dd0
A1GX <49387206+a1gx@users.noreply.github.com>
2022-09-29 20:32:59 +0800

    Update lualine.lua
    设置下标文件名显示相对路径

--- nvim/lua/plugin-config/lualine.lua
+++ nvim/lua/plugin-config/lualine.lua
@@ -15,7 +15,7 @@ lualine.setup({
   extensions = { "nvim-tree", "toggleterm" },
   sections = {
     lualine_c = {
-      "filename",
+      "%f",
       {
         "lsp_progress",
         spinner_symbols = { " ", " ", " ", " ", " ", " " },
